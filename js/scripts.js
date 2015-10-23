3b6799f5c029c9ee1b3983724f60363e3f0b9cdc
Jack Waibel <jackwaibel@gmail.com>
2015-10-23 16:22:48 -0700

    add javascript to make 99 bottles work

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,10 @@ var bottlesSong = function(bottles) {
       return false;
     }
   };
+
+
+$(document).ready(function() {
+    for (var bottles = 99; bottles >= 1; bottles -= 1) {
+      $("#lyrics").append("<p>" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer. Take one down, pass it around " + (bottles - 1) + " bottles of beer on the wall." + "</p>");
+    }
+});
